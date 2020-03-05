9d76f34e00c5052cb8058a417247980579fa42bf
Daniel Ribeiro Santiago <daniel.santiago@ufu.br>
2020-03-05 20:11:33 -0300

    Adiciona Exportação
    Adiciona a opção de exportação das polares em arquivos .csv .dat .mat e .xls , com a opção de escolha do tipo de caractere separador como "," ";" "tab". Para a exportação da geometria do aerofólio atual da a opção de incluir ou não a coluna "Side"

--- AeroSQL_Explorer.m
+++ AeroSQL_Explorer.m
@@ -1,7 +1,16 @@
 clear
 clc
 
-global AeroSQL_explorer Airfoils
+global AeroSQL_explorer Airfoils dir_exp delimiter form_exp
+
+%% Diretório para armazenamento dos arquivos exportados padrão
+
+dir_exp = cd;
+
+%% Formato e delimitador padrão para exportação
+
+delimiter = ";";
+form_exp = "csv";
 
 %% Conexão com o banco de dados
 
@@ -16,16 +25,45 @@ Airfoils = fetch(conn,"SELECT * FROM Airfoils;");
 % Janela Principal
 AeroSQL_explorer.main_figure = uifigure();
 AeroSQL_explorer.main_figure.Name = 'AeroSQL Database Explorer';
-AeroSQL_explorer.main_figure.Position = [400 350 1100 700];
+AeroSQL_explorer.main_figure.Position = [400 250 1100 730];
 
 % Listbox com perfis disponíveis
 AeroSQL_explorer.Perfis_listbox = uilistbox(AeroSQL_explorer.main_figure,'Items',Airfoils.Name,'ItemsData',1:length(Airfoils.Name));
 AeroSQL_explorer.Perfis_listbox.Position = [20 100 330 AeroSQL_explorer.main_figure.Position(4)-140];
 AeroSQL_explorer.Perfis_listbox.Tag = "perfis disponiveis";
-AeroSQL_explorer.Perfis_listbox.ValueChangedFcn = @(listbox,event) muda_lista(listbox,conn,event);
+AeroSQL_explorer.Perfis_listbox.ValueChangedFcn = @(listbox,event) muda_lista(listbox,event);
 
 % Label indicando o que se trata a listbox
 AeroSQL_explorer.label_Perfis_disp = uilabel(AeroSQL_explorer.main_figure,'Text','Perfis Disponíveis','Position',[20 AeroSQL_explorer.Perfis_listbox.Position(2)+AeroSQL_explorer.Perfis_listbox.Position(4)-5 100 25]);
+
+
+% ---------------------------------------------------------------------- Menu ---------------------------------------------------------------------------------
+
+AeroSQL_explorer.menu_opcoes_principal = uimenu(AeroSQL_explorer.main_figure,'Text',"&Opções");
+
+% menu opções de exportação
+AeroSQL_explorer.menu_opcoes_exp = uimenu(AeroSQL_explorer.menu_opcoes_principal, 'Text',"&Opções de Exportação");
+
+AeroSQL_explorer.menu_opcoes_exp_dir_exp = uimenu(AeroSQL_explorer.menu_opcoes_exp, 'Text',"&Diretório de Exportação");
+AeroSQL_explorer.menu_opcoes_exp_dir_exp.MenuSelectedFcn = @(menu, events) Seleciona_Menu(menu);
+
+AeroSQL_explorer.menu_opcoes_exp_formato_arquivo =  uimenu(AeroSQL_explorer.menu_opcoes_exp, 'Text',"&Formato dos arquivos");
+AeroSQL_explorer.menu_opcoes_exp_formato_arquivo.MenuSelectedFcn = @(menu, events) Seleciona_Menu(menu);
+
+% menu Exportar
+AeroSQL_explorer.menu_exportar = uimenu(AeroSQL_explorer.menu_opcoes_principal,'Text',"&Exportar");
+
+AeroSQL_explorer.menu_exportar_geometria = uimenu(AeroSQL_explorer.menu_exportar, 'Text',"&Geometria");
+AeroSQL_explorer.menu_exportar_geometria.MenuSelectedFcn = @(menu, events) Seleciona_Menu(menu);
+
+AeroSQL_explorer.menu_exportar_Polar = uimenu(AeroSQL_explorer.menu_exportar, 'Text',"&Polar");
+AeroSQL_explorer.menu_exportar_Polar_atual = uimenu(AeroSQL_explorer.menu_exportar_Polar, 'Text', "Polar Atual");
+AeroSQL_explorer.menu_exportar_Polar_atual.MenuSelectedFcn = @(menu, events) Seleciona_Menu(menu);
+
+AeroSQL_explorer.menu_exportar_Polar_todas = uimenu(AeroSQL_explorer.menu_exportar_Polar, 'Text', "Todas as Polares do Perfil Atual");
+AeroSQL_explorer.menu_exportar_Polar_todas.MenuSelectedFcn = @(menu, events) Seleciona_Menu(menu);
+
+% ---------------------------------------------------------------------- Tabs ---------------------------------------------------------------------------------
 
 % TabGroup Para disponibilizar informações do perfil
 AeroSQL_explorer.Perfis_tabgroup = uitabgroup(AeroSQL_explorer.main_figure);
@@ -78,9 +116,11 @@ AeroSQL_explorer.dropdown_coeficiente.ValueChangedFcn = @(dropdown,event) Seleci
 
 %% Funções
 
-function muda_lista(listbox,conn,event)
+function muda_lista(listbox,event)
 
-global AeroSQL_explorer Airfoils Polares
+global AeroSQL_explorer Airfoils Polares geometria_perfil
+
+conn = database('AeroSQLDB_MySQL_Reader','Reader','');
 
 if listbox.Tag == "perfis disponiveis"
 	
@@ -112,7 +152,7 @@ if listbox.Tag == "perfis disponiveis"
 	AeroSQL_explorer.dropdown_Reynolds.ItemsData = 1:length(Polares.Source.n_crit(1).Mach(1).Reynolds(1).Value);
 	AeroSQL_explorer.aux_Reynolds = 1;
 	
-		
+	AeroSQL_explorer.aux_Coeficiente = "Cl";
 	plot(AeroSQL_explorer.axes_coeficientes,Polares.Source.n_crit(1).Mach(1).Reynolds(1).Polar(1).Value.Alpha,Polares.Source.n_crit(1).Mach(1).Reynolds(1).Polar(1).Value.Cl);
 	text(AeroSQL_explorer.axes_coeficientes,0.5,0.9,Airfoils.Name{listbox.Value},'Units','normalized','FontWeight','bold','FontSize',25,'HorizontalAlignment','Center')
 	
@@ -125,7 +165,7 @@ end
 
 function Seleciona_dropdown(dropdown,event)
 
-global AeroSQL_explorer Polares Airfoils
+global AeroSQL_explorer Polares Airfoils delimiter_dropdown
 
 if dropdown.Tag == "Source"
 	
@@ -229,6 +269,155 @@ elseif dropdown.Tag == "Coeficiente"
 	end
 	
 	text(AeroSQL_explorer.axes_coeficientes,0.5,0.9,Airfoils.Name{AeroSQL_explorer.aux_Perfil},'Units','normalized','FontWeight','bold','FontSize',25,'HorizontalAlignment','Center')
+
+elseif dropdown.Tag == "Fmt arquivo possiveis"
+	
+	if dropdown.Value == 2 || dropdown.Value == 3
+		delimiter_dropdown.Enable = 'off';
+	else
+		delimiter_dropdown.Enable = 'on';
+	end
+
+end
+
+end
+
+function Seleciona_Menu(menu)
+
+global AeroSQL_explorer Airfoils dir_exp form_exp delimiter inc_side geometria_perfil form_aquivo_dlg_box delimiter_dropdown Polares
+
+if menu.Text == "&Diretório de Exportação"
+	
+	dir_exp = uigetdir(cd,"Selecione um diretório para salvar os dados");
+	
+elseif menu.Text == "&Formato dos arquivos"
+	
+	opc_possiveis = ["csv","xls","mat","dat"];
+	delimiters_possiveis = [",",";","tab"];
+	
+	form_aquivo_dlg_box = uifigure('Name',"Informe o formato do arquivo de exportação desejado");
+	form_aquivo_dlg_box.Position = [600 550 350 150];
+	
+	fmt_dropdown = uidropdown(form_aquivo_dlg_box,'Items',opc_possiveis,'ItemsData',1:length(opc_possiveis));
+	fmt_dropdown.Position = [form_aquivo_dlg_box.Position(3)/2-120 100 100 25];
+	fmt_dropdown.ValueChangedFcn = @(dropdown,event) Seleciona_dropdown(dropdown,event);
+	fmt_dropdown.Tag = "Fmt arquivo possiveis";
+	
+	delimiter_dropdown = uidropdown(form_aquivo_dlg_box,'Items',delimiters_possiveis,'ItemsData',1:length(delimiters_possiveis));
+	delimiter_dropdown.Position = [fmt_dropdown.Position(1)+145 100 100 25];
+	delimiter_dropdown.ValueChangedFcn = @(dropdown,event) Seleciona_dropdown(dropdown,event);
+	delimiter_dropdown.Tag = "Delimiter possiveis";
+	
+	inc_side_chkbox = uicheckbox(form_aquivo_dlg_box,'Text',"Incluir coluna 'Side' no arquivo de geometria");
+	inc_side_chkbox.Position = [form_aquivo_dlg_box.Position(3)/2-130 delimiter_dropdown.Position(2)-35 300 25];
+	inc_side_chkbox.ValueChangedFcn = @(chkbox,event) Muda_checkbox(chkbox,event);
+	
+	salvar = uibutton(form_aquivo_dlg_box,'Text','Salvar','Tag',"Salvar Fmt arquivo",'ButtonPushedFcn',@(btn,event) Aperta_Botao(btn, opc_possiveis(fmt_dropdown.Value), delimiters_possiveis( delimiter_dropdown.Value )));
+	salvar.Position = [form_aquivo_dlg_box.Position(3)/2-125 20 110 25];
+	
+	cancelar = uibutton(form_aquivo_dlg_box,'Text','Cancelar','Tag',"Cancelar Fmt arquivo",'ButtonPushedFcn',@(btn,event) Aperta_Botao(btn));
+	cancelar.Position = [salvar.Position(1)+145 salvar.Position(2) 110 25];
+	
+
+elseif menu.Text == "&Geometria"
+	
+	if form_exp == "csv" || form_exp == "dat"
+		if inc_side == 1
+			writetable(geometria_perfil,[dir_exp,'\',Airfoils.File_name{AeroSQL_explorer.Perfis_listbox.Value},'.',char(form_exp)],'Delimiter',char(delimiter))
+		else
+			writetable(geometria_perfil(:,1:2),[dir_exp,'\',Airfoils.File_name{AeroSQL_explorer.Perfis_listbox.Value},'.',char(form_exp)],'Delimiter',char(delimiter))
+		end
+	elseif form_exp == "xls"
+		writetable(geometria_perfil,[dir_exp,'\',Airfoils.File_name{AeroSQL_explorer.Perfis_listbox.Value},'.',char(form_exp)])
+	elseif form_exp == "mat"
+		eval([Airfoils.File_name{AeroSQL_explorer.Perfis_listbox.Value},'_geo',' = geometria_perfil;'])
+		writetable(geometria_perfil,[dir_exp,'\',Airfoils.File_name{AeroSQL_explorer.Perfis_listbox.Value},'.',char(form_exp)])
+	end
+	
+elseif menu.Text == "Polar Atual"
+	
+	Source = num2str(Polares.Source.Value(AeroSQL_explorer.aux_Source));
+	n_crit = num2str(Polares.Source.n_crit(AeroSQL_explorer.aux_Source).Value(AeroSQL_explorer.aux_n_crit));
+	Mach = num2str(Polares.Source.n_crit(AeroSQL_explorer.aux_Source).Mach(AeroSQL_explorer.aux_n_crit).Value(AeroSQL_explorer.aux_Mach));
+	Reynolds = num2str(Polares.Source.n_crit(AeroSQL_explorer.aux_Source).Mach(AeroSQL_explorer.aux_n_crit).Reynolds(AeroSQL_explorer.aux_Mach).Value(AeroSQL_explorer.aux_Reynolds));
+	
+	if form_exp == "csv" || form_exp == "dat"
+		writetable(Polares.Source.n_crit(AeroSQL_explorer.aux_Source).Mach(AeroSQL_explorer.aux_n_crit).Reynolds(AeroSQL_explorer.aux_Mach).Polar(AeroSQL_explorer.aux_Reynolds).Value(:,3:end),[dir_exp,'\',Airfoils.File_name{AeroSQL_explorer.Perfis_listbox.Value},'_',Source,'_',n_crit,'_',Mach,'_',Reynolds,'.',char(form_exp)],'Delimiter',char(delimiter));
+	elseif form_exp == "xls"
+		writetable(Polares.Source.n_crit(AeroSQL_explorer.aux_Source).Mach(AeroSQL_explorer.aux_n_crit).Reynolds(AeroSQL_explorer.aux_Mach).Polar(AeroSQL_explorer.aux_Reynolds).Value(:,3:end),[dir_exp,'\',Airfoils.File_name{AeroSQL_explorer.Perfis_listbox.Value},'_',Source,'_',n_crit,'_',Mach,'_',Reynolds,'.',char(form_exp)]);
+	elseif form_exp == "mat"
+		Polar = Polares.Source.n_crit(AeroSQL_explorer.aux_Source).Mach(AeroSQL_explorer.aux_n_crit).Reynolds(AeroSQL_explorer.aux_Mach).Polar(AeroSQL_explorer.aux_Reynolds).Value(:,3:end);
+		eval([Airfoils.File_name{AeroSQL_explorer.Perfis_listbox.Value},'_',Source,'_',n_crit,'_',Mach,'_',Reynolds,' = Polar;'])
+		save([dir_exp,'\',Airfoils.File_name{AeroSQL_explorer.Perfis_listbox.Value},'_',Source,'_',n_crit,'_',Mach,'_',Reynolds,'.',char(form_exp)],[Airfoils.File_name{AeroSQL_explorer.Perfis_listbox.Value},'_',Source,'_',n_crit,'_',Mach,'_',Reynolds])
+	end
+		
+elseif menu.Text == "Todas as Polares do Perfil Atual"
+
+	if form_exp == "csv" || form_exp == "dat"
+		mkdir([dir_exp,'\',Airfoils.File_name{AeroSQL_explorer.Perfis_listbox.Value}])
+		for i_Source = 1:length(Polares.Source.Value)
+			Source = num2str(Polares.Source.Value(i_Source));
+			for i_n_crit = 1:length(Polares.Source.n_crit(i_Source).Value)
+				n_crit = num2str(Polares.Source.n_crit(i_Source).Value(i_n_crit));
+				for i_Mach = 1:length(Polares.Source.n_crit(i_Source).Mach(i_n_crit).Value)
+					Mach = num2str(Polares.Source.n_crit(i_Source).Mach(i_n_crit).Value(i_Mach));
+					for i_Re = 1:length(Polares.Source.n_crit(i_Source).Mach(i_n_crit).Reynolds(i_Mach).Value)
+						Reynolds = num2str(Polares.Source.n_crit(i_Source).Mach(i_n_crit).Reynolds(i_Mach).Value(i_Re));
+						writetable(Polares.Source.n_crit(i_Source).Mach(i_n_crit).Reynolds(i_Mach).Polar(i_Re).Value(:,3:end),[dir_exp,'\',Airfoils.File_name{AeroSQL_explorer.Perfis_listbox.Value},'\',Airfoils.File_name{AeroSQL_explorer.Perfis_listbox.Value},'_',Source,'_',n_crit,'_',Mach,'_',Reynolds,'.',char(form_exp)],'Delimiter',char(delimiter));
+					end
+				end
+			end
+		end
+	elseif form_exp == "xls"
+		mkdir([dir_exp,'\',Airfoils.File_name{AeroSQL_explorer.Perfis_listbox.Value}])
+		for i_Source = 1:length(Polares.Source.Value)
+			Source = num2str(Polares.Source.Value(i_Source));
+			for i_n_crit = 1:length(Polares.Source.n_crit(i_Source).Value)
+				n_crit = num2str(Polares.Source.n_crit(i_Source).Value(i_n_crit));
+				for i_Mach = 1:length(Polares.Source.n_crit(i_Source).Mach(i_n_crit).Value)
+					Mach = num2str(Polares.Source.n_crit(i_Source).Mach(i_n_crit).Value(i_Mach));
+					for i_Re = 1:length(Polares.Source.n_crit(i_Source).Mach(i_n_crit).Reynolds(i_Mach).Value)
+						Reynolds = num2str(Polares.Source.n_crit(i_Source).Mach(i_n_crit).Reynolds(i_Mach).Value(i_Re));
+						writetable(Polares.Source.n_crit(AeroSQL_explorer.aux_Source).Mach(AeroSQL_explorer.aux_n_crit).Reynolds(AeroSQL_explorer.aux_Mach).Polar(AeroSQL_explorer.aux_Reynolds).Value(:,3:end),[dir_exp,'\',Airfoils.File_name{AeroSQL_explorer.Perfis_listbox.Value},'\',Airfoils.File_name{AeroSQL_explorer.Perfis_listbox.Value},'_',Source,'_',n_crit,'_',Mach,'_',Reynolds,'.',char(form_exp)]);
+					end
+				end
+			end
+		end		
+	elseif form_exp == "mat"
+		eval([Airfoils.File_name{AeroSQL_explorer.Perfis_listbox.Value},' = Polares;'])
+		save([dir_exp,'\',Airfoils.File_name{AeroSQL_explorer.Perfis_listbox.Value},'_',Source,'_',n_crit,'_',Mach,'_',Reynolds,'.',char(form_exp)],[Airfoils.File_name{AeroSQL_explorer.Perfis_listbox.Value}])
+	end
+	
+end
+
+end
+
+function Aperta_Botao(btn,varargin)
+
+global form_exp delimiter form_aquivo_dlg_box
+
+if btn.Tag == "Salvar Fmt arquivo"
+	
+	form_exp = varargin{1};
+	delimiter = varargin{2};
+	close(form_aquivo_dlg_box)
+	
+elseif btn.Tag == "Cancelar Fmt arquivo"
+	
+	close(form_aquivo_dlg_box)
+	
+end
+
+end
+
+function Muda_checkbox(chkbox,event)
+
+global inc_side
+
+if chkbox.Text == "Incluir coluna 'Side' no arquivo de geometria"
+	
+	inc_side = event.Value;
+	
 end
 
 end
